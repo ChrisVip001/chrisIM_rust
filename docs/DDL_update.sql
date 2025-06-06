9a7e5f594ff4b8bb395ed52dbe88596831cff449
chenmingliang <chenmliang@foxmail.com>
2025-06-06 22:26:26 +0800

    feat(group_member_settings): 添加显示群昵称设置
    在 group_member_settings 表中新增 show_nickname列，用于控制是否显示群昵称。同时在 DDL_update.sql 和 2025_06_06.sql 文件中添加相应的字段和注释。

--- docs/DDL_update.sql
+++ docs/DDL_update.sql
@@ -134,9 +134,11 @@ ALTER TABLE group_member_settings
 ADD COLUMN remark VARCHAR(255), -- 群备注
 ADD COLUMN is_pinned INTEGER NOT NULL DEFAULT 0, -- 群置顶，1-是，0-否
 ADD COLUMN recall_notification INTEGER NOT NULL DEFAULT 0; -- 群撤回通知，1-是，0-否
+ADD COLUMN show_nickname INTEGER NOT NULL DEFAULT 1; -- 是否显示群昵称，1-是，0-否
 
 COMMENT ON COLUMN group_member_settings.remark IS '群备注';
 COMMENT ON COLUMN group_member_settings.is_pinned IS '群置顶，1-是，0-否';
 COMMENT ON COLUMN group_member_settings.recall_notification IS '群撤回通知，1-是，0-否';
+COMMENT ON COLUMN group_member_settings.show_nickname IS '是否显示群昵称，1-是，0-否';
 
 ----------------------------250606  End----------------------------
