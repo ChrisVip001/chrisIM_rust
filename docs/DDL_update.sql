ed39f4b8fa1364bb1ce448901c6ecbe29b8819d0
chenmingliang <chenmliang@foxmail.com>
2025-06-07 02:41:55 +0800

    style:修复 SQL 语句中的分号位置
    - 在 group_member_settings 表的 ALTER TABLE语句中
    - 将 recall_notification 列的定义语句末尾的分号改为逗号- 保持与其他列定义一致的格式

--- docs/DDL_update.sql
+++ docs/DDL_update.sql
@@ -133,7 +133,7 @@ alter table group_members alter column role drop default;
 ALTER TABLE group_member_settings
 ADD COLUMN remark VARCHAR(255), -- 群备注
 ADD COLUMN is_top INTEGER NOT NULL DEFAULT 0, -- 群置顶，1-是，0-否
-ADD COLUMN recall_notification INTEGER NOT NULL DEFAULT 0; -- 群撤回通知，1-是，0-否
+ADD COLUMN recall_notification INTEGER NOT NULL DEFAULT 0, -- 群撤回通知，1-是，0-否
 ADD COLUMN show_nickname INTEGER NOT NULL DEFAULT 1; -- 是否显示群昵称，1-是，0-否
 
 COMMENT ON COLUMN group_member_settings.remark IS '群备注';
